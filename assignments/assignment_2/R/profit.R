311cea72d0c1b7255a65c0e2fe4d2d9f3d8ac5d3
scoutcleonard <88209342+scoutcleonard@users.noreply.github.com>
2022-04-19 12:40:46 -0700

    assignment 3

--- assignments/assignment_2/R/profit.R
+++ assignments/assignment_2/R/profit.R
@@ -1,7 +1,17 @@
-#' Profit Function
+#' Almond Profit Function
+#'
+#' This function calculates the profit generated from almond yield
+#' @param average_yield List of yearly yields from almond_yield function (tons/acre)
+#' @param price Average price in ($/ton)
+#' @param anomaly almond anomaly output from almond_yield function (tons/acre)
+#' @return yearly profit ($)
 #' 
-#' This function computes almond profit ($)
-#' @param anomaly almond yield anomaly (tons/acre)
-#' @param yield almond yield  (tons/acre/year)
-#' @param market_val
-#' 
+#' Equation
+
+profit <- function(anomaly, average_yield, price){
+  
+  money <- (average_yield + anomaly) * price
+  
+  return(money)
+  
+}
